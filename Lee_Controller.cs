e9167a46e667b7301794bc9b49975743571e275d
garrettwong99 <garrettwong99@gmail.com>
2020-05-26 21:47:34 -0700

    game controller

---
+++ Lee_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lee_Controller : MonoBehaviour
+{
+ public Rigidbody2D lee;
+ public Animator anim; 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            lee.velocity = new Vector2(-5,lee.velocity.y);
+            transform.localScale = new Vector2(-1,1);
+            anim.SetBool("running",true);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            lee.velocity = new Vector2(5,lee.velocity.y);
+            transform.localScale = new Vector2(1,1);
+            anim.SetBool("running",true);
+        }
+        else{
+            anim.SetBool("running",false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            lee.velocity = new Vector2(lee.velocity.x,10);
+        }
+
+    }
+}
